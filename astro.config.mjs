3495c8cb9fb067333e60c5938850eafaa7359e07
Sebastian Galindo <sebastian.galindo143@gmail.com>
2026-01-12 00:17:33 -0500

    Fix: Blog config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    outDir: 'dist',
+    output: "static",
+    outDir: "dist",
     site: "https://talkysafe143.github.io",
+    base: "/",
+    trailingSlash: "always",
     integrations: [
         react({
             experimentalReactChildren: true,
